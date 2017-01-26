7f8041b08c96d178cd3a4da156867c79b85dab98
K2487 <K2487@OHJE-330-WS-10>
2017-01-26 15:28:59 +0200

    tehtavat

---
+++ pesukone/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pesukone
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                Pesukone pesukone = new Pesukone();
+                pesukone.OnkoPäällä = true;
+                pesukone.Pikapesu = true;
+                pesukone.Linkous = 50;
+                pesukone.Lämpötila = 90;
+            Console.WriteLine("Linkous on " + pesukone.Linkous);
+            Console.WriteLine("Lämpötila on " + pesukone.Lämpötila);
+        }
+    }
+}
