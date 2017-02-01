c02631ade6a7105988cae9b3a6d610b5f96896d6
Olli <Olli@OLLI-PC>
2017-02-01 22:29:39 +0200

    teht04

---
+++ teht04/VehicleApplication/Vehicle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VehicleApplication
+{
+    class Vehicle
+    {
+        public string Name { get; set; }
+        public int Speed { get; set; }
+        public int Tyres { get; set; }
+
+        public void PrintData()
+        {
+            Console.WriteLine("Name: " + Name);
+            Console.WriteLine("Speed: " + Speed);
+            Console.WriteLine("Tyres: " + Tyres);
+        }
+
+        public void ToString()
+        {
+            string data = (Name + " " + Speed + " " + Tyres);
+            Console.WriteLine(data);
+        }
+    }
+}
